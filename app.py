859883225caf23213ec414bdf856044eeebed5a7
Replit user <>
2023-05-17 09:08:28 +0000

    Add templates(have to fix)

--- app.py
+++ app.py
@@ -2,10 +2,31 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+JOBS = [{
+  "id": 1,
+  "title": "Frontend Developer",
+  "location": "Hyderabad, India",
+  "salary": "12,00,000"
+}, {
+  "id": 2,
+  "title": "Backend Developer",
+  "location": "Hyderabad, India",
+  "salary": "10,00,000"
+}, {
+  "id": 1,
+  "title": "App Developer",
+  "location": "Delhi, India",
+}, {
+  "id": 1,
+  "title": "Full Stack Developer",
+  "location": "Hyderabad, India",
+  "salary": "20,00,000"
+}]
+
 
 @app.route("/")
 def main():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS)
 
 
 if __name__ == "__main__":
